c5aa4b5f9024e32b2b7d1e03aa88197c18254dae
HafezArtist <hdshafez@gmail.com>
2024-12-01 21:15:30 +0330

    added selection sort and bubble sort to the repo

---
+++ sort.py
@@ -0,0 +1,48 @@
+def selectionSort(arr):
+
+    #length of the list:
+    n = len(arr)
+
+    #iterating the list for the indexes:
+    for i in range(n):
+
+        #imagining that the i index is the smallest number:
+        minIndex = i
+
+        #iterating the list for the lowest value:(i + 1 is for checking the next index, n is for the list length):
+        for j in range(i + 1, n):
+
+            #if the iterated value is smaller that the minimum value shown in minIndex, change the index i, to j:
+            if arr[j] < arr[minIndex]:
+
+                minIndex = j
+
+        #if minimum index wasn't i, change the place of i and j:
+        if minIndex != i:
+            arr[i], arr[minIndex] = arr[minIndex], arr[i]
+
+    return arr
+
+
+def bubbleSort(arr):
+
+    n = len(arr)
+
+    for i in range(n):
+
+        swapped = False
+
+        for j in range(0, n - i - 1):
+
+            if arr[j] > arr[j + 1]:
+
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+
+        if not swapped:
+            break
+
+    return arr
+
+
+print(bubbleSort([15, 20, 8, 6, 40, 100]))
