3eb17e90224f06af61895febe0415a0f3dbdc0f4
HafezArtist <hdshafez@gmail.com>
2024-12-05 10:49:20 +0330

    added merge sort to the sort.py

--- sort.py
+++ sort.py
@@ -51,14 +51,61 @@ def bubbleSort(arr):
 
 #insertion sort
 
-A = [1, 6, 7, 4, 8, 5, 3, 2, 9]
+# A = [1, 6, 7, 4, 8, 5, 3, 2, 9]
 
-for k in range(1, len(A)):
-    item = A[k]
-    i = k
-    while i > 0 and A[i - 1] > item:
-        A[i] = A[i - 1]
-        i -= 1
-    A[i] = item
-    print(A)
-print(A)
+# for k in range(1, len(A)):
+#     item = A[k]
+#     i = k
+#     while i > 0 and A[i - 1] > item:
+#         A[i] = A[i - 1]
+#         i -= 1
+#     A[i] = item
+#     print(A)
+# print(A)
+
+
+def mergeSort(arr):
+    if len(arr) > 1:   #deviding into groups
+
+        mid = len(arr) // 2
+
+        #deviding into left and right groups:
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        mergeSort(left_half)
+        mergeSort(right_half)
+
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+
+                arr[k] = left_half[i]
+                i += 1
+
+            else:
+
+                arr[k] = right_half[j]
+                j += 1
+
+            k += 1
+
+        while i < len(left_half):
+
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+
+
+array = [38, 27, 43, 3, 9, 82, 10]
+mergeSort(array)
+print("Sorted array:", array)
